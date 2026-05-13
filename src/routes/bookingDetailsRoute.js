a291ea0b931b7c33ef34b538d6e63c0932cd5464
Maan <mehrankhan6540@gmail.com>
2026-05-14 00:04:03 +0500

    fix model import path

--- src/routes/bookingDetailsRoute.js
+++ src/routes/bookingDetailsRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import BookingDetails from "../models/BookingDetails.js";
+import BookingDetails from "../models/bookingDetails.js";
 
 const router = express.Router();
 
